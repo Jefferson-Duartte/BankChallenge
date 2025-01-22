bf11cf65099874e5a88d74f16022e49957208f2a
Jefferson Duarte <duartejefferson123@gmail.com>
2025-01-22 16:46:46 -0300

    build: add dependencies in pom.xml

---
+++ src/main/java/br/com/compass/model/User.java
@@ -0,0 +1,34 @@
+package br.com.compass.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private String cpf;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private TipoConta tipoConta;
+
+}
---
+++ src/main/java/br/com/compass/model/enums/AccountType.java
@@ -0,0 +1,9 @@
+package br.com.compass.model.enums;
+
+public enum AccountType {
+
+    CONTA_CORRENTE,
+    CONTA_SALARIO,
+    CONTA_POUPANCA
+
+}
