ac0376287ffed2eff99b39387129d261a7dd38d4
Jefferson Duarte <duartejefferson123@gmail.com>
2025-01-25 18:56:57 -0300

    feat: closing the EntityManagerFactory when finishing the application

--- src/main/java/br/com/compass/App.java
+++ src/main/java/br/com/compass/App.java
@@ -1,11 +1,16 @@
 package br.com.compass;
 
 import br.com.compass.ui.Menu;
+import br.com.compass.util.JpaUtil;
 
 public class App {
 
     public static void main(String[] args) {
-        Menu.run();
+        try {
+            Menu.run();
+        } finally {
+            JpaUtil.closeEntityManagerFactory();
+        }
     }
 
 }
