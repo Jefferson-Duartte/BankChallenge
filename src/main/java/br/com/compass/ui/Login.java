f017278131cdb1cde93d26fc02c947f493efb413
Jefferson Duarte <duartejefferson123@gmail.com>
2025-01-27 19:06:51 -0300

    feat: create selectAccountForManagement method for user to select the account he wants to manage

--- src/main/java/br/com/compass/ui/Login.java
+++ src/main/java/br/com/compass/ui/Login.java
@@ -1,15 +1,18 @@
 package br.com.compass.ui;
 
+import br.com.compass.model.BankAccount;
 import br.com.compass.model.Customer;
 import br.com.compass.service.CustomerService;
 
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Login {
 
     private static CustomerService customerService = new CustomerService();
 
-    public static boolean login(Scanner scanner) {
+    public static void login(Scanner scanner) {
         System.out.print("Enter your cpf: ");
         String inputCpf = scanner.nextLine();
         System.out.print("Enter your password: ");
@@ -19,13 +22,44 @@ public class Login {
             Customer loggedCustomer = customerService.login(inputCpf, inputPassword);
             System.out.println("Welcome, " + loggedCustomer.getName() + "!");
 
-            System.out.println(customerService.loadCustomerAccounts(loggedCustomer));
+            BankAccount selectedAccount = selectAccountForManagement(loggedCustomer, scanner);
 
+            if (selectedAccount != null) {
+                BankMenu.show(scanner, selectedAccount);
+            }
 
-            return true;
         } catch (IllegalArgumentException e) {
             System.out.println("Login failed: " + e.getMessage());
         }
-        return false;
     }
+
+    private static BankAccount selectAccountForManagement(Customer customer, Scanner scanner) {
+        List<BankAccount> accounts = customerService.loadCustomerAccounts(customer);
+
+        if (!accounts.isEmpty()) {
+
+            if (accounts.size() == 1) {
+                return accounts.get(0);
+            }
+
+            System.out.println("Please select the account you want to manage");
+            for (int i = 0; i < accounts.size(); i++) {
+                System.out.println(i + 1 + " - " + accounts.get(i));
+            }
+
+            System.out.print("Choose an option: ");
+            try {
+                int accountOption = Integer.parseInt(scanner.nextLine()) - 1;
+                return accounts.get(accountOption);
+
+            } catch (InputMismatchException | NumberFormatException | IndexOutOfBoundsException e) {
+                System.out.println("Invalid input! Please enter a number.");
+                return selectAccountForManagement(customer, scanner);
+            }
+
+        }
+
+        return null;
+    }
+
 }
