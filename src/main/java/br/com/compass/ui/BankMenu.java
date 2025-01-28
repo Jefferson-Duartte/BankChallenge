fb37ca17194f755d90a6d9121162c389a097c8a0
Jefferson Duarte <duartejefferson123@gmail.com>
2025-01-28 00:58:10 -0300

    feat: implements deposit, withdraw and validateAmount methods in BankMenu class

--- src/main/java/br/com/compass/ui/BankMenu.java
+++ src/main/java/br/com/compass/ui/BankMenu.java
@@ -1,19 +1,30 @@
 package br.com.compass.ui;
 
+import br.com.compass.model.BankAccount;
+import br.com.compass.service.AccountService;
+import br.com.compass.util.ValidationUtil;
+
+import java.math.BigDecimal;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BankMenu {
 
-    public static void show(Scanner scanner) {
+    private static AccountService accountService = new AccountService();
+
+
+    public static void show(Scanner scanner, BankAccount account) {
         boolean running = true;
 
         while (running) {
             System.out.println("========= Bank Menu =========");
+            System.out.println("========= Account selected: " + account + " =========");
             System.out.println("|| 1. Deposit              ||");
             System.out.println("|| 2. Withdraw             ||");
             System.out.println("|| 3. Check Balance        ||");
             System.out.println("|| 4. Transfer             ||");
             System.out.println("|| 5. Bank Statement       ||");
+            System.out.println("|| 6. Open New Account     ||");
             System.out.println("|| 0. Exit                 ||");
             System.out.println("=============================");
             System.out.print("Choose an option: ");
@@ -21,12 +32,11 @@ public class BankMenu {
             int option = Integer.parseInt(scanner.nextLine());
             switch (option) {
                 case 1:
-                    // ToDo...
-                    System.out.println("Deposit.");
+                    deposit(account, scanner);
                     break;
                 case 2:
                     // ToDo...
-                    System.out.println("Withdraw.");
+                    withdraw(account, scanner);
                     break;
                 case 3:
                     // ToDo...
@@ -40,16 +50,72 @@ public class BankMenu {
                     // ToDo...
                     System.out.println("Bank Statement.");
                     break;
+                case 6:
+                    // ToDo...
+                    System.out.println("Open new account.");
+                    break;
                 case 0:
                     // ToDo...
                     System.out.println("Exiting...");
                     running = false;
-                    return;
+                    break;
                 default:
                     System.out.println("Invalid option! Please try again.");
             }
         }
     }
 
+    private static void deposit(BankAccount account, Scanner scanner) {
+
+        BigDecimal amount = validateAmount(scanner);
+
+        System.out.println("Do you confirm the deposit of R$" + amount.toString().replace(".", ",") + " in your account " + account);
+
+        if (ValidationUtil.confirmedOperation(scanner)) {
+            accountService.deposit(account, amount);
+        } else {
+            System.out.println("Operation cancelled");
+            BankMenu.show(scanner, account);
+        }
+    }
+
+    private static void withdraw(BankAccount account, Scanner scanner) {
+        BigDecimal amount = validateAmount(scanner);
+
+        System.out.println("Do you confirm the withdraw of " + amount + " in your account " + account);
+
+        if (ValidationUtil.confirmedOperation(scanner)) {
+            accountService.withdraw(account, amount);
+
+        } else {
+            System.out.println("Operation cancelled");
+            BankMenu.show(scanner, account);
+        }
+    }
+
+
+    private static BigDecimal validateAmount(Scanner scanner) {
+
+        BigDecimal amount = null;
+
+        while (amount == null) {
+
+            System.out.print("Enter the amount: ");
+
+            try {
+                amount = BigDecimal.valueOf(Double.parseDouble(scanner.nextLine().replace(",", ".")));
+                if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+                    System.out.println("The value must be greater than zero. Please try again.");
+                    amount = null;
+                }
+            } catch (InputMismatchException | NumberFormatException e) {
+                System.out.println("Invalid input! Please enter a number.");
+            }
+
+        }
+
+        return amount;
+
+    }
 
 }
