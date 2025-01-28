ff37c98878ec16a784eeb836360a1684ecd448e6
Jefferson Duarte <duartejefferson123@gmail.com>
2025-01-28 09:05:51 -0300

    feat: create Transaction class

---
+++ src/main/java/br/com/compass/model/Transaction.java
@@ -0,0 +1,40 @@
+package br.com.compass.model;
+
+import br.com.compass.model.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Customer sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private Customer receiver;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+}
