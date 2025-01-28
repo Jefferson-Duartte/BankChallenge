198b51e6563715d7d43a00ecf8e3098956a2ee11
Jefferson Duarte <duartejefferson123@gmail.com>
2025-01-28 00:57:09 -0300

    feat: implements withdraw logic in AccountService class

--- src/main/java/br/com/compass/service/AccountService.java
+++ src/main/java/br/com/compass/service/AccountService.java
@@ -28,4 +28,29 @@ public class AccountService {
         System.out.println("Successful deposit");
     }
 
+    public void withdraw(BankAccount account, BigDecimal amount) {
+
+        BankAccount existingAccount = accountDAO.findById(account.getId());
+
+        if (existingAccount == null) {
+            System.out.println("Account not found");
+            return;
+        }
+
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            System.out.println("The value must be greater than zero");
+            return;
+        }
+
+        if (account.getBalance().compareTo(amount) < 0) {
+            System.out.println("Insufficient balance for withdrawal");
+            return;
+        }
+
+        existingAccount.setBalance(account.getBalance().subtract(amount));
+        accountDAO.update(existingAccount);
+        System.out.println("Successful withdraw");
+
+    }
+
 }
