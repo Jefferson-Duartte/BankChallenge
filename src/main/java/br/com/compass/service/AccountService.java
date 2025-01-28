0d11f48b2c7c1a0709adb94faa1babe90df020db
Jefferson Duarte <duartejefferson123@gmail.com>
2025-01-28 08:50:12 -0300

    feat: implements createAccount method in AccountService class

--- src/main/java/br/com/compass/service/AccountService.java
+++ src/main/java/br/com/compass/service/AccountService.java
@@ -1,13 +1,31 @@
 package br.com.compass.service;
 
 import br.com.compass.dao.AccountDAO;
+import br.com.compass.dao.CustomerDAO;
 import br.com.compass.model.BankAccount;
+import br.com.compass.model.Customer;
+import br.com.compass.model.enums.AccountType;
 
 import java.math.BigDecimal;
 
 public class AccountService {
 
     private AccountDAO accountDAO = new AccountDAO();
+    private CustomerDAO customerDAO = new CustomerDAO();
+
+    public BankAccount createAccount(Customer customer, AccountType accountType) {
+
+        BankAccount newAccount = new BankAccount();
+        newAccount.setAccountType(accountType);
+        newAccount.setCustomer(customer);
+
+        customer.getAccounts().add(newAccount);
+
+        customerDAO.update(customer);
+
+        return newAccount;
+
+    }
 
     public void deposit(BankAccount account, BigDecimal amount) {
 
@@ -42,20 +60,21 @@ public class AccountService {
             return;
         }
 
-        if (account.getBalance().compareTo(amount) < 0) {
+        if (existingAccount.getBalance().compareTo(amount) < 0) {
             System.out.println("Insufficient balance for withdrawal");
             return;
         }
 
-        existingAccount.setBalance(account.getBalance().subtract(amount));
+        existingAccount.setBalance(existingAccount.getBalance().subtract(amount));
         accountDAO.update(existingAccount);
         System.out.println("Successful withdraw");
 
     }
 
-    public BigDecimal checkBalance(BankAccount account){
+    public BigDecimal checkBalance(BankAccount account) {
         BankAccount existingAccount = accountDAO.findById(account.getId());
         return existingAccount.getBalance();
     }
+
 
 }
